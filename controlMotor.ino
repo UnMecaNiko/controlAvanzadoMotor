e28f3424eec136ba618dc95db8b71a4695fc2d26
Nico Velasquez <velasquezlopezn@gmail.com>
2022-09-14 16:59:19 -0500

    first commit

---
+++ controlMotor.ino
@@ -0,0 +1,15 @@
+
+
+hw_timer_t * timer = NULL;
+volatile uint8_t ledstat = 0; 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
