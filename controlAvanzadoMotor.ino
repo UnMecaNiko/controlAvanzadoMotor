3c319e60c36aaf5e7eba0d4838e0c4d39528dfa9
Nicolas Velasquez <velasquezlopezn@gmail.com>
2022-10-13 04:49:14 -0500

    control discreto continuo funcionando (corriente)

--- controlAvanzadoMotor.ino
+++ controlAvanzadoMotor.ino
@@ -59,10 +59,10 @@ float stepRef = slopeRef*sampleTime/1000000;
 float voltMotor = 0;
 float errorAct     = 0;
 
-float q0=   0;
-float q1=   0.0597;
-float q2=	  -0.0467;
-float s0=   -0.9887;
+float q0=   15.7769;
+float q1=   -15.6914;
+float q2=	  0;
+float s0=   -0.9958;
 
 float error[] ={0,0};
 float out[]   ={0,0};
